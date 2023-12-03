e023eff8386625faaf68876c4e7e30b34cd63a3e
Oğuzcan Karaman <oguzcan07karaman@gmail.com>
2023-12-03 16:18:58 +0300

    small bug fix in main

--- Main.java
+++ Main.java
@@ -5,27 +5,46 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int method = 0;
         System.out.println("to use calculator press      ---> 1");
         System.out.println("to use net calculator press  ---> 2");
         System.out.println("to use net sign/logger press ---> 3");
-        int method = in.nextInt();
-        switch (method){
+        if (in.hasNextInt()) {
+            method = in.nextInt();
+        } else {
+            System.out.println("you must press a number to process");
+            System.exit(0);
+        }
+        switch (method) {
             case 1:
                 System.out.println("Press 1 for add");
                 System.out.println("Press 2 for subtract");
                 System.out.println("Press 3 for multiply");
                 System.out.println("Press 4 for division");
                 int calculator = in.nextInt();
-                switch (calculator){
-                    case 1: Calculator.add(); break;
-                    case 2: Calculator.subtract(); break;
-                    case 3: Calculator.multiply(); break;
-                    case 4: Calculator.division(); break;
+                switch (calculator) {
+                    case 1:
+                        Calculator.add();
+                        break;
+                    case 2:
+                        Calculator.subtract();
+                        break;
+                    case 3:
+                        Calculator.multiply();
+                        break;
+                    case 4:
+                        Calculator.division();
+                        break;
                     default:
                         System.out.println("undefined");
-                } break;
-            case 2: NetCalculation.netCalculator(); break;
-            case 3: LogSign.logAndSign(); break;
+                }
+                break;
+            case 2:
+                NetCalculation.netCalculator();
+                break;
+            case 3:
+                LogSign.logAndSign();
+                break;
             default:
                 System.out.println("undefined");
         }
